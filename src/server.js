aa78b2a6bc80aa99df6823722024eee0583aac94
Farzad Fahiminia <ff222cb@student.lnu.se>
2022-01-31 13:22:38 +0100

    Add basic files

---
+++ src/server.js
@@ -0,0 +1,61 @@
+/**
+ * The starting point of the application.
+ *
+ * @author Farzad Fahiminia <ff222cb@student.lnu.se>
+ * @version 1.0.0
+ */
+
+import express from 'express'
+import expressLayouts from 'express-ejs-layouts'
+// import session from 'express-session'
+import logger from 'morgan'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+// import { router } from './routes/router.js'
+// import { connectDB } from './config/mongoose.js'
+import 'dotenv/config'
+
+console.log('Hello World!')
+console.log(process.env.PORT)
+
+// Creates an Express application.
+const app = express()
+
+// Get the directory name of this module's path.
+const directoryFullName = dirname(fileURLToPath(import.meta.url))
+
+// Set the base URL to use for all relative URLs in a document.
+const baseURL = process.env.BASE_URL || '/'
+
+// Set up a morgan logger using the dev format for log entries.
+app.use(logger('dev'))
+
+// View engine setup.
+app.set('view engine', 'ejs')
+app.set('views', join(directoryFullName, 'views'))
+app.use(expressLayouts)
+app.set('layout', join(directoryFullName, 'views', 'layouts', 'default'))
+
+// Parse requests of the content type application/x-www-form-urlencoded.
+// Populates the request object with a body object (req.body).
+app.use(express.urlencoded({ extended: false }))
+
+// Serve static files.
+app.use(express.static(join(directoryFullName, '..', 'public')))
+
+// Middleware to be executed before the routes.
+app.use((req, res, next) => {
+  // Pass the base URL to the views.
+  res.locals.baseURL = baseURL
+
+  next()
+})
+
+// Register routes.
+// app.use('/', router)
+
+// Starts the HTTP server listening for connections.
+app.listen(process.env.PORT, () => {
+  console.log(`Server running at http://localhost:${process.env.PORT}`)
+  console.log('Press Ctrl-C to terminate...')
+})

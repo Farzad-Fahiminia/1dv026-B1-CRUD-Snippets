84fb439d3c73bcdf5c938682a5013e3dfe2afd44
Farzad Fahiminia <ff222cb@student.lnu.se>
2022-02-01 13:59:23 +0100

    Add snippet visibility on snippets page

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -18,6 +18,6 @@ export class HomeController {
    * @param {Function} next - Express next middleware function.
    */
   index (req, res, next) {
-    res.render('layouts/default')
+    res.render('home/index')
   }
 }
--- src/routes/router.js
+++ src/routes/router.js
@@ -7,7 +7,15 @@
 
 import express from 'express'
 import { router as homeRouter } from './home-router.js'
+import { router as snippetsRouter } from './snippets-router.js'
 
 export const router = express.Router()
 
 router.use('/', homeRouter)
+router.use('/snippets', snippetsRouter)
+
+// router.use('*', (req, res, next) => {
+//   const error = new Error('Not Found')
+//   error.status = 404
+//   next(error)
+// })

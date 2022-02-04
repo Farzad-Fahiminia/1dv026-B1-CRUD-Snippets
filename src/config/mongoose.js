c26837b852afb18751300bea3da82cdbba4886c5
Farzad Fahiminia <ff222cb@student.lnu.se>
2022-02-04 09:02:52 +0100

    Lint fix

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -8,10 +8,10 @@
 import mongoose from 'mongoose'
 
 /**
-* Establishes a connection to a database.
-*
-* @returns {Promise} Resolves to this if connection succeeded.
-*/
+ * Establishes a connection to a database.
+ *
+ * @returns {Promise} Resolves to this if connection succeeded.
+ */
 export const connectDB = async () => {
   const { connection } = mongoose
 
